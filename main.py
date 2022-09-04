c915a4c66d69e9825b925f2f23bd4008a0d227c6
ath4nasis <romanafonin@protonmail.com>
2022-09-04 11:07:28 +0200

    Working on schedule exe

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import sched
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
 driver.get("http://www.python.org")
-print(driver.title)
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def main(sc):
+    print(driver.title)
+    sc.enter(30, 1, main, (sc,))
+
+
+if __name__ == '__main__':
+    s.enter(30, 1, main, (s,))
+    s.run()
