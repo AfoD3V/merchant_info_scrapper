dda38178f831a0125a4b9044f993e499632defb5
ath4nasis <romanafonin@protonmail.com>
2022-09-04 14:28:04 +0200

    Scrapper function scrape_data  for retrieving html from dynamically generated content

--- scraper.py
+++ scraper.py
@@ -9,11 +9,11 @@ from bs4 import BeautifulSoup
 
 
 class Scraper:
-    def __init__(self, url):
+    def __init__(self, url: str = "https://lostmerchants.com/"):
+        # URL of site where do want to proceed scrap
         self.url = url
 
     def scrape_data(self, region_name: str = "EU Central", server_name: str = "Procyon") -> str:
-
         # Chrome settings
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
@@ -21,19 +21,18 @@ class Scraper:
         chrome_options.add_argument("--no-sandbox")
 
         # URL and driver settings
-        url = 'https://lostmerchants.com/'
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get(self.url)
 
-        # Waiting for all content on page to load
+        # Waiting for drop_down menu content to load
         driver.implicitly_wait(5)
 
-        # Region selection
+        # Region selection if not set in params than going "EU central" by default
         region = driver.find_element(By.ID, 'severRegion')
         drp_region = Select(region)
         drp_region.select_by_visible_text(region_name)
 
-        # Server selection
+        # Server selection if not set in params than going "Procyon" by default
         server = driver.find_element(By.ID, 'server')
         drp_region = Select(server)
         drp_region.select_by_visible_text(server_name)
@@ -47,5 +46,5 @@ class Scraper:
         return str(soup)
 
 
-c = Scraper("https://lostmerchants.com/")
+c = Scraper()
 print(c.scrape_data())
