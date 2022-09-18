50ec73638593b64910e20bf51d3e9ed6943a5822
ath4nasis <romanafonin@protonmail.com>
2022-09-18 13:17:56 +0200

    Preparing for deploy

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-import sched
-import time
+from fastapi import FastAPI
 
-s = sched.scheduler(time.time, time.sleep)
-
-def main(sc):
-    # Do something
-    print("driver.title")
-
-    # Intervals for function in sec
-    sc.enter(10, 1, main, (sc,))
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    # Initial sched starter
-    s.enter(1, 1, main, (s,))
-    s.run()
-
-# TEST
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
